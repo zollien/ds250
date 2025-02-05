719f5a57c2d23ba4d2234edf9c1855fe5cda4ab0
zollien <dov19003@byui.edu>
2025-02-05 07:12:09 +0000

    W03

---
+++ ds_setup/1.py
@@ -0,0 +1,5 @@
+#%%
+# copy paste this into a python file in vs code and run this cell
+import sys
+!{sys.executable} -m pip install numpy pandas scikit-learn lets-plot palmerpenguins nbformat nbclient pyyaml
+# %%
